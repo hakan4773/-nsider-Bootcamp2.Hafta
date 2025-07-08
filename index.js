6773d1ee33a35dcc355c927b15f37a0d72ddcf47
hakan4773 <hb480312@gmail.com>
2025-07-08 16:24:33 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const user = {
 };
 
 let cart = [];
-
+//ekleme
 function addProduct() {
   const name = prompt("Ürün adı:");
   const price = parseFloat(prompt("Ürün fiyatı:"));
@@ -13,18 +13,24 @@ function addProduct() {
     cart.push({ name, price });
   }
 }
-
+//listeleme
 function listCart() {
   cart.forEach(item => {
     console.log(`${item.name} - ${item.price} TL`);
   });
 }
-
+//toplam fiyat hesaplmama
 function calculateTotal() {
   const total = cart.reduce((sum, item) => sum + item.price, 0);
   console.log(`Toplam: ${total} TL`);
 }
-
+//dinamik ekleme
+let adding = true;
+while (adding) {
+  addProduct();
+  adding = prompt("Devam? (evet/hayır)") === "evet";
+}
+//silme
 function removeProduct(name) {
   const index = cart.findIndex(item => item.name.toLowerCase() === name.toLowerCase());
   if (index !== -1) {
