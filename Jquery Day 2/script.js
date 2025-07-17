95ade19e4da83a05f72b24c06d388b9e3a4304ff
hakan4773 <hb480312@gmail.com>
2025-07-17 17:11:28 +0300

    Jquery ve Ajax kullanarak veri çekme işlemi  yapıldı

---
+++ Jquery Day 2/script.js
@@ -0,0 +1,71 @@
+const posts=$(".container");
+let start = 0;
+const limit = 5;
+isLoading=false;
+function getPosts(){
+        isLoading = true;
+$('#loading').show(); 
+$(".scroll").text("Yükleniyor...");
+
+$.ajax({
+  url: `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`,
+  method: "GET",
+  success: function (response) {
+    if (start >= 100) return;
+    
+    response.forEach(function (post) {
+$('.container').append(
+  `<div id="postContainer">
+     <p><strong>Post ID:</strong> ${post.id}</p>
+     <p><strong>User ID:</strong> ${post.userId}</p>
+     <p class="name"><strong>Title:</strong> ${post.title}</p>
+     <p><strong>Body:</strong> ${post.body}</p>
+   </div>`
+);
+         });
+start += limit;
+      $('#loading').hide();
+          isLoading = false;
+
+  },
+  error: function (xhr, status, error) {
+          $(".scroll").text("Yükleme hatası. Tekrar deneyin...");
+
+  },
+    complete: function() {
+      $('#loading').hide();
+      isLoading = false;
+    }
+});
+}
+
+$(document).ready(function(){
+
+
+getPosts();   
+
+  let scrollTimer;
+
+$(window).scroll(function () {
+      clearTimeout(scrollTimer);
+scrollTimer=setTimeout(function(){
+
+ if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+getPosts(); 
+
+  }
+
+},300
+)
+ 
+
+});
+
+
+
+
+
+
+
+
+});
